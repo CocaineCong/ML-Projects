9963bdbd61ca4348d02c6c7737d3410e0053b9ed
fanone <294350394@qq.com>
2021-09-01 21:30:16 +0800

    feat:add knn

--- ex2(knn)/knn1.py
+++ ex2(knn)/knn1.py
@@ -1,1 +1,45 @@
+"""
+Sklearn中的make_circles方法生成训练样本
+并随机生成测试样本，用KNN分类并可视化。
+"""
 
+from sklearn.datasets import make_circles
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+fig = plt.figure(1, figsize=(10, 5))
+x1, y1 = make_circles(n_samples=400, factor=0.4, noise=0.1)
+# 模型训练 求距离、取最小K个、求类别频率
+knn = KNeighborsClassifier(n_neighbors=15)
+knn.fit(x1, y1)  # X是训练集(横纵坐标) y是标签类别
+
+# 进行预测
+x2 = random.random()  # 测试样本横坐标
+y2 = random.random()  # 测试样本纵坐标
+
+X_sample = np.array([[x2, y2]])  # 给测试点
+# y_sample = knn.predict(X_sample)   # 调用knn进行predict得预测类别
+y_sample = []
+for i in range(0, 400):
+    dx = x1[:, 0][i] - x2
+    dy = x1[:, 1][i] - y2
+    d = (dx ** 2 + dy ** 2) ** 1 / 2
+    y_sample.append(d)
+
+neighbors = knn.kneighbors(X_sample, return_distance=False)
+
+plt.subplot(121)
+plt.title('data by make_circles() 1')
+plt.scatter(x1[:, 0], x1[:, 1], marker='o', s=100, c=y1)
+plt.scatter(x2, y2, marker='*', c='b')
+
+plt.subplot(122)
+plt.title('data by make_circles() 2')
+plt.scatter(x1[:, 0], x1[:, 1], marker='o', s=100, c=y1)
+plt.scatter(x2, y2, marker='*', c='r', s=100)
+for i in neighbors[0]:
+    plt.scatter([x1[i][0], X_sample[0][0]], [x1[i][1], X_sample[0][1]], marker='o', c='b', s=100)
+
+plt.show()
---
+++ ex2(knn)/knn2.py
@@ -0,0 +1,37 @@
+"""
+Sklearn中的datasets方法导入训练样本
+并用留一法产生测试样本
+用KNN分类并输出分类精度
+"""
+import warnings
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import LeaveOneOut
+import matplotlib.pyplot as plt
+warnings.filterwarnings('ignore')
+
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+loo = LeaveOneOut()  # 留一法
+
+K = []
+Accuracy = []
+for k in range(1, 16):
+    correct = 0
+    knn = KNeighborsClassifier(k)
+    for train, test in loo.split(X):  # 对测试机和训练集进行分割
+        knn.fit(X[train], y[train])
+        y_sample = knn.predict(X[test])
+        if y_sample == y[test]:
+            correct += 1
+    K.append(k)
+    Accuracy.append(correct / len(X))
+    plt.plot(K, Accuracy)
+    plt.xlabel('Accuracy：')
+    plt.ylabel('K：')
+    print('K次数:{} Accuracy正确率:{}'.format(k, correct / len(X)))
+
+plt.show()
